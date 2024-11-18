da184d510f530893b0fbcd35f5e547265b454d57
TemoJokhadze <jokh1jokhadze02@gmail.com>
2024-11-18 16:51:15 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,160 @@
+// Functions
+
+//task 1
+
+// function numbs (p1,p2){
+//  return(p1 + p2);
+// }
+// console.log(numbs(7,9));
+
+//task 2
+
+// function length(p1){
+//     return(p1.length);
+// }
+// console.log(length("Hello world"));
+
+//task 3
+
+// function odd_even(p1){
+//     if((p1%2) == 0){
+//        return("This number is odd");
+       
+//     }else {
+//         return("This number is even");
+//     }
+// }
+// console.log(odd_even(19));
+
+
+//task 4
+
+// const arr = [1,2,3,4]
+// function firstElement() {
+//      return console.log(arr[0])
+// }
+// firstElement()
+
+
+//task 5
+
+// const object = {
+//      name: "mercedes",
+//      engine: 5.5,
+//      color: "black"
+// }
+// function obj (){
+//      return(console.log(Object.values(object)))
+// }
+// obj()
+
+
+
+// Arrow functions
+
+//task 1
+
+// const arrow = parameter1 => {
+//     return(parameter1 ** 2)
+// }
+// console.log(arrow(5));
+
+//task 2
+
+// const arrow = p1 => {
+//      return p1
+// }
+// console.log(arrow("Hello"));
+
+
+// task 3
+
+// const arrow = (p1,p2) => {
+//     if(p1 > p2){
+//      console.log(p1);
+//     }else if(p2 > p1){
+//      console.log(p2);
+//     }else{
+//      console.log("These numbers are equal");
+//     }
+// }
+// arrow(2,3);
+
+
+// task 4
+
+// const arr = [1,3,5,8]
+
+// const arrow = p1 => {
+    //  const sum = p1.reduce(
+    //       (accumulator, currentValue) => accumulator + currentValue,
+    //  )
+    //  console.log(sum);
+     
+// }
+// arrow(arr);
+
+
+// Arrays
+
+//task 1
+
+// const arr = [4,8,3]
+// console.log(arr[0]);
+
+
+//task 2
+
+// const arr = [4,8,3]
+// arr.push(9)
+// console.log(arr);
+
+//task 3 
+
+// const arr = [4,8,3]
+// arr.shift()
+// console.log(arr);
+
+//task 5
+ 
+// const arr = [3,6,9]
+
+// arr.forEach((element) => console.log(element))
+
+
+// combined tasks
+
+// task 1
+
+// const obj = {
+//     name: "mustang",
+//     engine: 2.3
+// }
+
+// function car (){
+//     console.log(obj.name,obj.engine);
+// }
+// car()
+
+// task 3
+
+// let arr = [3,4,16,24]
+
+// const arrow = p1 =>{
+//     const sum = p1.reduce(
+//         (accumulator, currentValue) => accumulator + currentValue,
+//    )
+//    console.log(sum);
+// }
+// arrow(arr)
+
+// task 4
+
+// const obj = {
+//     name: "Temo",
+//     job: "Web-developer"
+// }
+
+// console.log(`Hello, am ${obj.name} and I am ${obj.job} for 4 years.`);
+
+
